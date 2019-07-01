8cf98fe930c52937e4f412cd9c46727350a495e9
Cris Smith <cris.smith.fa@gmail.com>
2019-07-01 16:57:56 -0500

    almost there

--- public/index.js
+++ public/index.js
@@ -9,7 +9,6 @@ function Products(products){
             <div>${product.name}</div>
             <img src=${product.imgUrl}>
             <button class="details">Product Details</button>
-            <button class="select">Select</button>
             <br>
             <br>
         </div>
@@ -25,11 +24,13 @@ document.getElementById("cart-btn").addEventListener('click',viewCart);
 document.getElementById("add-btn").addEventListener('click',addToCart);
 document.getElementById("cart").addEventListener('click', removeItem);
 document.getElementById("container").addEventListener('click',showDetails);
+document.getElementById("checkout-btn").addEventListener('click',showCheckout);
 
 let searchbox = document.getElementById("searchbox");
 let resultsbox = document.getElementById("resultsbox");
 let cart = document.getElementById("cart");
-let addBtn=document.getElementById("add-btn");
+let checkout = document.getElementById("checkout");
+//let addBtn=document.getElementById("add-btn");
 
 //search the products:
 //CLASS NOTES: THINK ABOUT USING FILTER (products.filter)
@@ -79,8 +80,31 @@ function showDetails(e){
         description.innerHTML = `
         <div>Full Product Description: ${products[0].description}</div>
         <div>Catagory: ${products[0].category}</div>
-        <div>Rating:${products[0].rating}</div>
-        <div>Price:${products[0].price}</div>`;
+        <div>Rating: ${products[0].rating}</div>
+        <div>Price: ${products[0].price}</div>
+        <div>Reviews: ${products[0].reviews[0].description}</div>
+        <button id="add-btn">Add to cart</button>
+        <select id="pick-amt">
+            <option value="default">--Qty--</option>
+            <option value="1">Qty: 1</option>
+            <option value="2">Qty: 2</option>
+            <option value="3">Qty: 3</option>
+            <option value="4">Qty: 4</option>
+            <option value="5">Qty: 5</option>
+            <option value="6">Qty: 6</option>
+            <option value="7">Qty: 7</option>
+            <option value="8">Qty: 8</option>
+            <option value="9">Qty: 9</option>
+            <option value="10">Qty: 10</option>
+        </select>`;
         e.target.parentElement.appendChild(description)
     }
 }
+
+//show checkout form:
+
+function showCheckout(e){
+    e.preventDefault();
+    checkout.style.display = 'block'
+}
+
